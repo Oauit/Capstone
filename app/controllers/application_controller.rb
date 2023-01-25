a0203e0d9f746bb895968fa2c1fa4f8d49972caf
oauit <aidanjade.ward@gmail.com>
2023-01-24 18:52:22 -0700

    Rails setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+# class ApplicationController < ActionController::API
+#     include ActionController::Cookies
+
+#     def hello_world 
+#         session[:count] = (session[:count] || 0) + 1
+#         render json: {count :session[:count] }
+#     end
+# end
+
+# app/controllers/application_controller.rb
+class ApplicationController < ActionController::API
+    include ActionController::Cookies
+  
+    def hello_world
+      session[:count] = (session[:count] || 0) + 1
+      render json: { count: session[:count] }
+    end
+end
